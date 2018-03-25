7c0d00f5be7341cc5efdc4a7d56abaf553280bc9
laibulle <laibulle@gmail.com>
2018-03-25 18:17:28 +0200

    update mix

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Warpex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       description: "Wrap10 client",
+      package: package(),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps()
